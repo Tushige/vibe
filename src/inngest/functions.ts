b2adb49914d8de09854090bc803ff3cf2f88df11
Tushige <Tushige@users.noreply.github.com>
2025-09-10 08:18:45 -0500

    set up inggest agentkit

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,24 @@
 import { inngest } from "./client";
+import { openai, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const summarizer = createAgent({
+      name: "summarizer",
+      system: "You are an expert summarizer. You summarize in 3 words.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    // Run the agent with an input.  This automatically uses steps
+    // to call your AI model.
+    const { output } = await summarizer.run(`summarize the following text: ${event.data.value}`);
+    console.log(output)
+    return {
+      success: 'ok'
+    }
+  }
 );
