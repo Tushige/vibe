1c169b57956d81b2b25c271e248de0e02131aa0d
Tushige <Tushige@users.noreply.github.com>
2025-09-17 08:17:59 -0500

    added messaging to our agent

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,14 +1,8 @@
-
-'use client'
+"use client";
 import { useTRPC } from "@/trpc/client";
-import {  useSuspenseQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 export default function ClientHome() {
   const trpc = useTRPC();
-  const {data} = useSuspenseQuery(trpc.hello.queryOptions({text: 'hello world'}))
 
-  return (
-    <div className="font-sans ">
-      {JSON.stringify(data)}
-    </div>
-  );
+  return <div className="font-sans "></div>;
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -4,12 +4,18 @@ import {
   createAgent,
   createTool,
   createNetwork,
+  type Tool,
 } from "@inngest/agent-kit";
 import { Sandbox } from "@e2b/code-interpreter";
 import { getSandbox, lastAssistantTextMessageContent } from "./utils";
 import z from "zod";
 import { PROMPT } from "@/prompt";
+import { prisma } from "@/lib/db";
 
+interface AgentState {
+  summary: string;
+  files: { [path: string]: string };
+}
 // Schema for the terminal command input
 const terminalInputSchema = z.object({
   command: z.string().describe("The terminal command to execute"),
@@ -39,9 +45,9 @@ const readSchema = z.object({
     })
   ),
 });
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+export const codeAgentFunc = inngest.createFunction(
+  { id: "code-agent" },
+  { event: "code-agent/run" },
   async ({ event, step }) => {
     const sandboxId = await step.run("get-sandbox-id", async () => {
       const sandbox = await Sandbox.create("vibe-test-nextjs-minner775");
@@ -51,7 +57,7 @@ export const helloWorld = inngest.createFunction(
       return sandbox.sandboxId;
     });
     // Create a new agent with a system prompt (you can add optional tools, too)
-    const codeAgent = createAgent({
+    const codeAgent = createAgent<AgentState>({
       name: "code-agent",
       description: "An Expert coding agent",
       system: PROMPT,
@@ -92,7 +98,10 @@ export const helloWorld = inngest.createFunction(
           name: "createOrUpdateFile",
           description: "Create or Update files in the sandbox",
           parameters: createOrUpdateSchema,
-          handler: async ({ files }, { step, network }) => {
+          handler: async (
+            { files },
+            { step, network }: Tool.Options<AgentState>
+          ) => {
             const result = await step?.run("createOrUpdateFiles", async () => {
               try {
                 const updatedFiles = network.state.data.files || {};
@@ -148,7 +157,7 @@ export const helloWorld = inngest.createFunction(
       },
     });
 
-    const network = createNetwork({
+    const network = createNetwork<AgentState>({
       name: "coding-agent-network",
       agents: [codeAgent],
       maxIter: 3,
@@ -167,6 +176,9 @@ export const helloWorld = inngest.createFunction(
     });
     const result = await network.run(event.data.value);
 
+    const isError =
+      !result.state.data.summary ||
+      Object.keys(result.state.data.files || {}).length == 0;
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
       try {
         const sandbox = await getSandbox(sandboxId);
@@ -175,6 +187,32 @@ export const helloWorld = inngest.createFunction(
       } catch (err) {
         console.error(err);
       }
+    });
+
+    await step.run("save-result", async () => {
+      if (isError) {
+        return await prisma.message.create({
+          data: {
+            content: "Error: Something went wrong. Please try again.",
+            role: "ASSISTANT",
+            type: "ERROR",
+          },
+        });
+      }
+      return await prisma.message.create({
+        data: {
+          content: result.state.data.summary,
+          role: "ASSISTANT",
+          type: "RESULT",
+          fragment: {
+            create: {
+              url: sandboxUrl || "",
+              title: "files",
+              files: result.state.data.files,
+            },
+          },
+        },
+      });
     });
 
     return {
